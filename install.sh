1d4ea83c49b2ff00995bf4736b629adf41518a8e
phire <me@phire.cc>
2021-06-16 17:12:56 +0200

    Add root access check

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
 mkdir -p /usr/share/wgnord
 cp template.conf countries.txt /usr/share/wgnord
 cp wgnord /bin/
