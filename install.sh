b69fd07fcff74ccc69afb128de1842693a4aaa6d
phire <me@phire.cc>
2021-06-29 23:37:48 +0200

    Use /var/lib/wgnord and fix permissions

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "This script must be run as root"
 	exit 1
 fi
-conf_dir=/usr/share/wgnord
+conf_dir=/var/lib/wgnord
 mkdir -p $conf_dir
+chmod 770 $conf_dir
 cp template.conf countries.txt $conf_dir
 cp wgnord /bin/
