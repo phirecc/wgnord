a38ea5346e15f3301169fcf212fc4f25225ce92e
phire <me@phire.cc>
2021-06-29 23:32:22 +0200

    conf_dir in install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "This script must be run as root"
 	exit 1
 fi
-mkdir -p /usr/share/wgnord
-cp template.conf countries.txt /usr/share/wgnord
+conf_dir=/usr/share/wgnord
+mkdir -p $conf_dir
+cp template.conf countries.txt $conf_dir
 cp wgnord /bin/
