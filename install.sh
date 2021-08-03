94f3a050b74fd546e4d7c92ccc39f3923732cb75
phire <me@phire.cc>
2021-08-03 02:17:05 +0200

    DESTDIR in install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ if [ "$(id -u)" -ne 0 ]; then
 	echo "This script must be run as root"
 	exit 1
 fi
-conf_dir=/var/lib/wgnord
+conf_dir=$DESTDIR/var/lib/wgnord
 mkdir -p $conf_dir
 chmod 770 $conf_dir
 cp template.conf countries.txt $conf_dir
-cp wgnord /bin/
+cp wgnord $DESTDIR/bin/
